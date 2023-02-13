a94a36d0b654a6920c2cda8f2fdccf79b79497c4
ssunbear <117508164+ssunbear@users.noreply.github.com>
2023-02-13 23:38:22 +0900

    [Silver III] Title: 병든 나이트, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1783. 병든 나이트/병든 나이트.py
@@ -0,0 +1,12 @@
+from sys import stdin
+
+n,m = map(int, stdin.readline().split())
+
+if n == 1:
+    print(1)
+elif n == 2:
+    print(min(4, (m-1)//2+1))
+elif m <= 6:
+    print(min(4, m))
+else:
+    print(m-2)
