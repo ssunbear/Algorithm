0a677c8e0c0f7168f1280a994ae08b6aa1715ee3
ssunbear <117508164+ssunbear@users.noreply.github.com>
2023-02-14 00:50:19 +0900

    [Gold IV] Title: 수 묶기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.py
+++ 백준/Gold/1744. 수 묶기/수 묶기.py
@@ -1,36 +1,30 @@
 import sys
+input=sys.stdin.readline
 
 n=int(input())
-pos=[] # 양수를 저장할 리스트
-neg=[] # 0과 음수를 저장할 리스트 (0을 음수취급)
+pos=[] 
+neg=[]
 total=0
 
 for i in range(n):
-    num=int(sys.stdin.readline())
-    if num>1:
-        pos.append(num)
-    elif num==1: # 1은 양수와 음수모두 만나도 더하기만 진행
-        total+=1
-    else:
-        neg.append(num)
-        
+    data=int(input())
+    if data>1: pos.append(data)
+    elif data==1: total+=1
+    else: neg.append(data)
+
 pos.sort(reverse=True)
 neg.sort()
 
 if len(pos)%2==0:
-    for i in range(0, len(pos), 2):
-        total+=pos[i]*pos[i+1]
+    for i in range(0, len(pos), 2): total+=pos[i]*pos[i+1]
 else:
-    for i in range(0, len(pos)-1, 2):
-        total+=pos[i]*pos[i+1]
+    for i in range(0, len(pos)-1, 2): total+=pos[i]*pos[i+1]
     total+=pos[len(pos)-1]
 
 if len(neg)%2==0:
-    for i in range(0, len(neg), 2):
-        total+=neg[i]*neg[i+1]
+    for i in range(0, len(neg), 2): total+=neg[i]*neg[i+1]
 else:
-    for i in range(0, len(neg)-1, 2):
-        total+=neg[i]*neg[i+1]
+    for i in range(0, len(neg)-1, 2): total+=neg[i]*neg[i+1]
     total+=neg[len(neg)-1]
 
 print(total)
