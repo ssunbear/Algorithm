b38effc966c8fe589ff2257d1f3b5629abcb82a7
ssunbear <117508164+ssunbear@users.noreply.github.com>
2023-02-16 21:29:52 +0900

    [Silver III] Title: 삼각형 만들기, Time: 708 ms, Memory: 76132 KB -BaekjoonHub

---
+++ 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.py
@@ -0,0 +1,15 @@
+import sys
+input=sys.stdin.readline
+from itertools import combinations
+
+n=int(input())
+data=[]
+for i in range(n):
+    data.append(int(input()))
+data.sort(reverse=True)
+
+for i in range(n-2):
+    if data[i]<data[i+1]+data[i+2]:
+        print(data[i]+data[i+1]+data[i+2])
+        break
+else: print(-1)
