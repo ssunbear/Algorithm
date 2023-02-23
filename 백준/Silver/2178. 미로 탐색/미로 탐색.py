80d03d0fabbde8591fd50f687bfbacc6c5176f61
ssunbear <117508164+ssunbear@users.noreply.github.com>
2023-02-23 23:28:03 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -3,27 +3,26 @@ input = sys.stdin.readline
 from collections import deque
 
 n,m=map(int, input().split())
-graph=[]
-for i in range(n):
-    graph.append(list(map(int, input().strip())))
+graph=[list(map(int, input().strip())) for _ in range(n)]
+visited=[[False for i in range(m)] for _ in range(n)]
 
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
+dx=[-1,0,1,0]
+dy=[0,-1,0,1]
 
 def bfs(x,y):
     queue=deque()
-    queue.append((x,y))
+    queue.append([x,y])
     while queue:
         x, y= queue.popleft()
+        if x==n-1 and y==m-1:
+            print(graph[n-1][m-1])
+            return
         for i in range(4):
             nx, ny= x+dx[i], y+dy[i]
-            if nx<0 or ny<0 or nx>=n or ny>=m:
-                continue
-            if graph[x][y]==0:
-                continue
-            if graph[nx][ny]==1:
-                graph[nx][ny]=graph[x][y]+1
-                queue.append((nx,ny))
-    return graph[n-1][m-1]
+            if 0<=nx<n and 0<=ny<m:
+                if graph[nx][ny]==1 and not visited[nx][ny]:
+                    graph[nx][ny]=graph[x][y]+1
+                    visited[nx][ny]=True
+                    queue.append([nx,ny])
 
-print(bfs(0,0))    
+bfs(0,0)
