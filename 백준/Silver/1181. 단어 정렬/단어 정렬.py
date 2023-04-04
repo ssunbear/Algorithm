670b04a52432975149930ae5a2263501f002419b
ssunbear <117508164+ssunbear@users.noreply.github.com>
2023-04-04 18:48:34 +0900

    [Silver V] Title: 단어 정렬, Time: 96 ms, Memory: 38164 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,11 +1,13 @@
 import sys
 input=sys.stdin.readline
+
 n=int(input())
-data=[input().strip() for _ in range(n)]
-A=set(data)
-B={}
-for x in A:
-    B[x]=len(x)
-C=sorted(B.items(), key= lambda x: (x[1], x[0]))
-for x in C:
-    print(x[0])
+word=[input().strip() for _ in range(n)]
+A=set(word)
+dic={}
+for w in A:
+    dic[w]=len(w)
+
+dic1=sorted(dic.items(),  key=lambda x: (x[1],x[0]))
+for i in dic1:
+    print(i[0])
