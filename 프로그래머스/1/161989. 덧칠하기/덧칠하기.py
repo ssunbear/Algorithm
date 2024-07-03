af6904e98db578ab74ced916618b1741d35dc08e
Yoon Seonwoong <neuron4722@naver.com>
2024-07-03 11:48:07 +0900

    [level 1] Title: 덧칠하기, Time: 156.90 ms, Memory: 11.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -0,0 +1,9 @@
+def solution(n, m, section):
+    answer = 0
+    while section:
+        if len(section) > 0:
+            next_pos = section[0] + m
+            while section and section[0] < next_pos:
+                section.pop(0)
+            answer += 1
+    return answer
