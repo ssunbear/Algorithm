cc0d8fb2b3466df7ca4d739f5f281ef7e69efc92
Yoon Seonwoong <neuron4722@naver.com>
2024-07-12 14:10:24 +0900

    Update 과제 진행하기.py

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -1,45 +1,37 @@
-# 32분 11초
-
-# t의 범위를 엄청크게 설정하니까 통과함
-# "00:00" ~ "23:59" 사이의 시간값만 들어가 있습니다. 인데 t의 범위는 0 ~ 1440이 아닌가 ?? 어이없네
-
-import collections
-
-def timeConvertor(time):
-    (h, m) =  map(int, time.split(":"))
-    return h * 60 + m
-
 def solution(plans):
-    answer = []
-    plans.sort(key = lambda x : x[1])
-    
+    answer=[]
+    now=[]
+    #배열 시간순으로 정렬
+    #배열 수정: 시간을 모두 분으로 수정, 문자형 정수를 정수형으로 수정
+    plans.sort(key=lambda x:x[1])
     for i in range(len(plans)):
-        plans[i][1] = timeConvertor(plans[i][1])
-        plans[i][2] = int(plans[i][2])
+        plans[i][1]=int(plans[i][1][0:2])*60+int(plans[i][1][3:])
+        plans[i][2]=int(plans[i][2])
     
-    TaskWaitQueue = collections.deque()
-    idx = 0
+    for i in range(len(plans)-1):
+        #현재과제와 다음과제 차이 계산
+        now.append(plans[i])
+        diff=plans[i+1][1]-plans[i][1]
+        
+        #시간차이가 업거나 진행중인 과제가 없을때까지 반복
+        while now and diff:
+            #현재과제 해결 가능: 완료리스트(answer)에 추가, 진행중과제 pop
+            if now[-1][2]<=diff:
+                diff-=now[-1][2]
+                answer.append(now[-1][0])
+                now.pop()
+            #현재과제 해결 불가능: 진행중인 과제에 시간차이 업데이트
+            else: 
+                now[-1][2]-=diff
+                diff=0
     
-    currentTask = 0
-    for t in range(plans[0][1], 6000 * 24 + 102):
-        if idx + 1 < len(plans) and plans[idx + 1][1] == t:
-            if plans[currentTask][2] != 0:
-                TaskWaitQueue.append(currentTask)
-            
-            currentTask = idx + 1
-            idx += 1
-        elif plans[currentTask][2] == 0:
-            if TaskWaitQueue:
-                currentTask = TaskWaitQueue.pop()
-
-        if plans[currentTask][2] > 0:
-            plans[currentTask][2] -= 1
-            if plans[currentTask][2] == 0:
-                print("*** [end] Time: %d, TaskName = %s ***" %(t, plans[currentTask][0]))
-                answer.append(plans[currentTask][0])
-
+    #마지막과제와 남아있는 진행중인 과제 역순으로 저장
+    answer.append(plans[-1][0])
+    while now:
+        answer.append(now[-1][0])
+        now.pop(-1)
+    
     return answer
 
-plans = [["a", "09:00", "30"], ["b", "09:20", "10"], ["c", "09:40", "10"]]
-
-print(solution(plans))
+# 약 한시간 정도 소요
+# 시간복잡도 O(n*logn)
