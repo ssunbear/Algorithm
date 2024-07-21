0fffcd50b267055bf1e5f4a3d075668ed491970b
Yoon Seonwoong <neuron4722@naver.com>
2024-07-21 21:00:47 +0900

    [Gold II] Title: 후위 표기식, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -0,0 +1,27 @@
+list = list(input())
+stack=[]
+res=''
+    
+for x in list:
+    if x.isalpha():
+        res+=x
+    else:
+        if x == '(':
+            stack.append(x)
+        elif x == '*' or x == '/':
+            while stack and (stack[-1] == '*' or stack[-1] =='/'):
+                res += stack.pop()
+            stack.append(x)
+        elif x == '+' or x == '-':
+            while stack and stack[-1] != '(':
+                res+= stack.pop()
+            stack.append(x)
+        elif x == ')':
+            while stack and stack[-1] != '(':
+                res += stack.pop()
+            stack.pop()
+                    
+while stack:
+    res+=stack.pop()
+        
+print(res)
