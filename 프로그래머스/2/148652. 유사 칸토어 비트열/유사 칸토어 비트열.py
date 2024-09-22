b3114c3056bb35fd13154d2a50ba33758be95a43
Yoon Seonwoong <neuron4722@naver.com>
2024-09-22 21:47:32 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 197.79 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
@@ -1,51 +1,30 @@
-# 50qns?
-import sys
-sys.setrecursionlimit(int(1e8))
+def get_cnt_1(n, l, r):
+    if n == 1:
+        return "11011"[l:r + 1].count("1")
+
+    ranges = []
+
+    for i in range(5):
+        ranges.append(range(i * 5 ** (n - 1), (i + 1) * 5 ** (n - 1)))
+
+    ret = 0
+    for i in range(len(ranges)):
+        if ranges[i].start == 2 * 5 ** (n - 1):
+            continue
+
+        # 완전 포함
+        if l <= ranges[i].start and r >= ranges[i].stop:
+            ret += get_cnt_1(n - 1, 0, 5 ** (n - 1) - 1)
+        # 부분 포함
+        elif l <= ranges[i].start and ranges[i].start <= r < ranges[i].stop:
+            ret += get_cnt_1(n - 1, 0, r - ranges[i].start)
+        elif ranges[i].stop >= l > ranges[i].start and r >= ranges[i].stop:
+            ret += get_cnt_1(n - 1, l - ranges[i].start, 5 ** (n - 1) - 1)
+        elif l > ranges[i].start and r < ranges[i].stop:
+            ret += get_cnt_1(n - 1, l - ranges[i].start, r - ranges[i].start)
+
+    return ret
+
 def solution(n, l, r):
-    answer = 0
-    
-    # 5^nn 크기를 가진 문자열의 [ll,rr] 내의 1의 개수
-    def solve(nn,ll,rr):
-        # base case
-        if nn==1 :
-            ret = 0
-            for idx in range(ll,rr):
-                if not idx==2:
-                    ret+=1
-            return ret
-        
-        # logic
-        sum = 0
-        for i in range(5):
-            if i==2 :
-                continue
-            # [s,e)
-            s = (5**nn) // 5 * i
-            e = (5**nn) // 5 * (i+1) 
-        
-            # 올바르지 않은 구간
-            if ll >= e:
-                continue
-            left = max(s,ll) - s
-            
-            if rr < s:
-                continue
-            right = min(e,rr) - s
-
-            sum+=solve(nn-1,left,right)
-        return sum
-    # 폐구간을 반폐구간 [l,r+1)
-    answer = solve(n,l-1,r)
-        
-    return answer
-
-# 1
-# 11011 [2.2]
-# 1101111011000001101111011 [10,11,12,13,14] [10,15)
-# ()()(00000)()()
-# 단순 구현? -> 5^20 = 조 단위 X
-
-# 1,5,25,125 ...
-# n을 5등분할때, 가운데 구간은 전부 0이다.
-# 그 등분 중에서, 가운데 구간은 0이다!
-# 5^n, l, r 중 1의 개수를 반환하도록 하기
+    ret = get_cnt_1(n, l - 1, r - 1)
+    return ret
