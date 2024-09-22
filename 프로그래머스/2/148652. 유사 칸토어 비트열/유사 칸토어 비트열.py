27ae6b7e257534b7f4cd6ad125c278d887b5582e
Yoon Seonwoong <neuron4722@naver.com>
2024-09-22 21:40:37 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 137.15 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
@@ -1,19 +1,51 @@
-
-def check(i):
-    # i가 5로 나눈 나머지가 2인 경우 (0-based 인덱스에서 '0'으로 간주)
-    if i % 5 == 2:
-        return False
-    # i가 5보다 작은 경우 (0, 1, 3, 4는 '1'로 간주)
-    if i < 5:
-        return True
-    # 재귀적으로 5로 나누어 값 계산
-    return check(i // 5)
-
+# 50qns?
+import sys
+sys.setrecursionlimit(int(1e8))
 def solution(n, l, r):
     answer = 0
+    
+    # 5^nn 크기를 가진 문자열의 [ll,rr] 내의 1의 개수
+    def solve(nn,ll,rr):
+        # base case
+        if nn==1 :
+            ret = 0
+            for idx in range(ll,rr):
+                if not idx==2:
+                    ret+=1
+            return ret
+        
+        # logic
+        sum = 0
+        for i in range(5):
+            if i==2 :
+                continue
+            # [s,e)
+            s = (5**nn) // 5 * i
+            e = (5**nn) // 5 * (i+1) 
+        
+            # 올바르지 않은 구간
+            if ll >= e:
+                continue
+            left = max(s,ll) - s
+            
+            if rr < s:
+                continue
+            right = min(e,rr) - s
 
-    for i in range(l - 1, r):  # l과 r은 1-base이므로 0-base로 변환
-        if check(i):
-            answer += 1
-
+            sum+=solve(nn-1,left,right)
+        return sum
+    # 폐구간을 반폐구간 [l,r+1)
+    answer = solve(n,l-1,r)
+        
     return answer
+
+# 1
+# 11011 [2.2]
+# 1101111011000001101111011 [10,11,12,13,14] [10,15)
+# ()()(00000)()()
+# 단순 구현? -> 5^20 = 조 단위 X
+
+# 1,5,25,125 ...
+# n을 5등분할때, 가운데 구간은 전부 0이다.
+# 그 등분 중에서, 가운데 구간은 0이다!
+# 5^n, l, r 중 1의 개수를 반환하도록 하기
